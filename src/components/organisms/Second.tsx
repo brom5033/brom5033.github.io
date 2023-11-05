e4705c130ab4cce1c04daf93ab930fef92c16bdd
brom5033 <sjajdcjddlsl@naver.com>
2023-11-05 21:33:03 +0900

    feat: organisms 분류

---
+++ src/components/organisms/Second.tsx
@@ -0,0 +1,12 @@
+import { ColorSection } from '@components/atoms/ColorSection';
+import { Typography } from '@mui/material';
+
+export const Second = () => {
+  return (
+    <ColorSection color="#5D5DA8">
+      <Typography component="h1" variant="h1">
+        FRONTEND
+      </Typography>
+    </ColorSection>
+  );
+};
