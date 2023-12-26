2e1a174932b32cce534b480e4558006c2582675a
brom5033 <sjajdcjddlsl@naver.com>
2023-12-26 15:10:04 +0900

    style(pages): add type fc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import type { FC } from 'react';
 import { IndexTemplate } from '@components/templates';
 import { DrawerAppBar } from '@components/molecules';
 import { Footer } from '@components/organisms';
 
-export const Index = () => {
+export const Index: FC = () => {
   return (
     <>
       <DrawerAppBar />
